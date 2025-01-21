1eee450b566a59fe19d7df7f2cc1d47d9c77ecdd
GeneRickUsername <52056666+GeneRickUsername@users.noreply.github.com>
2025-01-20 22:51:11 -0600

    Mod1

--- PackagedPlugin/Source/ProceduralLandscape/Private/Component/GeoClipmapMeshComponent.cpp
+++ PackagedPlugin/Source/ProceduralLandscape/Private/Component/GeoClipmapMeshComponent.cpp
@@ -318,7 +318,7 @@ public:
 		// Check we have data 
 		if(	SectionData != nullptr) 			
 		{
-			FRHICommandListImmediate& RHICmdList = FRHICommandListExecutor::GetImmediateCommandList();;
+			FRHICommandListImmediate& RHICmdList = FRHICommandListExecutor::GetImmediateCommandList();
 			// Check it references a valid section
 			if (SectionData->TargetSection < Sections.Num() &&
 				Sections[SectionData->TargetSection] != nullptr)
